c72716bd54171106a5cb02c73d4c138b326f9d0a
wiufixo <104910435+wiufixo@users.noreply.github.com>
2022-07-06 09:24:11 +0900

    init

---
+++ NiOtNeOt3/src/main/java/com/sist/nono/NiOtNeOt3Application.java
@@ -0,0 +1,13 @@
+package com.sist.nono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiOtNeOt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiOtNeOt3Application.class, args);
+	}
+
+}
