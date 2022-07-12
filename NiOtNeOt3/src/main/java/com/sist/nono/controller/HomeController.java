3485e1301f0e470b6c2bd89dd0839e2fa1595c04
wiufixo <104910435+wiufixo@users.noreply.github.com>
2022-07-12 21:32:49 +0900

    user login,logout,update else...commit

--- NiOtNeOt3/src/main/java/com/sist/nono/controller/HomeController.java
+++ NiOtNeOt3/src/main/java/com/sist/nono/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.sist.nono.auth.PrincipalDetail;
@@ -48,32 +49,35 @@ public class HomeController {
 	}
 	
 	@GetMapping("/account/update")
-	public String update() {
+	public String update(Model model, @AuthenticationPrincipal PrincipalDetail principal) {
+		model.addAttribute("principal", principal);
 		return "account/update";
 	}
 	
 	@PostMapping("/account/update")
-	public void updateSubmit(User user) {
-		userService.save(user); // 회원정보 수정은 DB에 적용이 되지만 session값은 변경되지 않은 상태기 때문에 직접 세션 변경해줘야한다
+	public String updateSubmit(User user) {
+		
+		userService.update(user); // 회원정보 수정은 DB에 적용이 되지만 session값은 변경되지 않은 상태기 때문에 직접 세션 변경해줘야한다
 		//세션등록
 		Authentication authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(user.getCu_id(), user.getCu_pwd()));
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-	}
-	
-	//회원탈퇴
-	@GetMapping("/resign")
-	public String resign(Model model, @AuthenticationPrincipal PrincipalDetail principal) {
-		model.addAttribute("principal", principal);
-		System.out.println(principal.getPassword());
-		System.out.println(principal.getUsername());
-		System.out.println(principal.getUser().getCu_id());
-		System.out.println(principal.getUser().getCu_pwd());
-		return "account/resign";
-	}
-	//회원탈퇴
-	@PostMapping("/user/delete")
-	public String delete(User user) {
-		userService.resign(user);
 		return "redirect:/";
 	}
+	
+//	//회원탈퇴
+//	@GetMapping("/resign")
+//	public String resign(Model model, @AuthenticationPrincipal PrincipalDetail principal) {
+//		model.addAttribute("principal", principal);
+//		System.out.println(principal.getPassword());
+//		System.out.println(principal.getUsername());
+//		System.out.println(principal.getUser().getCu_id());
+//		System.out.println(principal.getUser().getCu_pwd());
+//		return "account/resign";
+//	}
+//	//회원탈퇴
+//	@PostMapping("/user/delete")
+//	public String delete(User user) {
+//		userService.resign(user);
+//		return "redirect:/";
+//	}
 }
--- NiOtNeOt3/src/main/java/com/sist/nono/repository/UserRepository.java
+++ NiOtNeOt3/src/main/java/com/sist/nono/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.sist.nono.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.sist.nono.model.Board;
@@ -15,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	
 	@Query(value = "select count(*) from user where cu_id=?1 and cu_pwd=?2", nativeQuery = true)
 	int findUser(String cu_id, String cu_pwd);
+	
+	@Modifying
+	@Query(value = "update user set cu_pwd=?1,cu_email=?2 where cu_no=?3", nativeQuery = true)
+	int update(String cu_pwd, String cu_email, int cu_no);
 }
